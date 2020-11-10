3cfb3283c67b5a5f681b068e27ba96f5de343254
valeriia-lun <61375793+valeriia-lun@users.noreply.github.com>
2020-11-10 17:20:58 +0200

    added comments

--- src/Output.java
+++ src/Output.java
@@ -1,47 +1,47 @@
 public class Output {
-    public static void output(int pointX, int pointY, double[][][] knowledge, int[][][] map){
+    public static void output(int pointX, int pointY, double[][][] knowledge, int[][][] map) {
         System.out.println("------------------------------------------------------------------------");
-        System.out.println("Current feelings in :"+ "("+pointX+";"+pointY+")");
+        System.out.println("Current feelings in :" + "(" + pointX + ";" + pointY + ")");
 
-        if(map[pointX][pointY][0] == 0 && map[pointX][pointY][1] == 0 && map[pointX][pointY][2] == 0) {
+        if (map[pointX][pointY][0] == 0 && map[pointX][pointY][1] == 0 && map[pointX][pointY][2] == 0) {
             System.out.println("Adjacent rooms are completely safe.");
         }
-        if(map[pointX][pointY][1] == 1) {
+        if (map[pointX][pointY][1] == 1) {
             System.out.println("Agent sensor: breeze");
         }
-        if(map[pointX][pointY][0] == 1) {
+        if (map[pointX][pointY][0] == 1) {
             System.out.println("Agent sensor: stench");
         }
-        if(map[pointX][pointY][2] == 1) {
+        if (map[pointX][pointY][2] == 1) {
             System.out.println("Agent sensor: glitter - GOLD!");
         }
 
         System.out.println("Predictions to fall in pit:");
-        if(pointY > 0) {
+        if (pointY > 0) {
             System.out.println("Up -> " + knowledge[pointX][pointY - 1][0]);
         }
-        if(pointY < 3) {
+        if (pointY < 3) {
             System.out.println("Down -> " + knowledge[pointX][pointY + 1][0]);
         }
-        if(pointX > 0) {
+        if (pointX > 0) {
             System.out.println("Left -> " + knowledge[pointX - 1][pointY][0]);
         }
-        if(pointX < 3) {
+        if (pointX < 3) {
             System.out.println("Right ->" + knowledge[pointX + 1][pointY][0]);
         }
 
         System.out.print("\nPredictions of finding Wumpus:");
-        if(pointY > 0) {
+        if (pointY > 0) {
             System.out.println("Up -> " + knowledge[pointX][pointY - 1][1]);
         }
-        if(pointY < 3) {
-            System.out.println("Down -> " + knowledge[pointX][pointY+ 1][1]);
+        if (pointY < 3) {
+            System.out.println("Down -> " + knowledge[pointX][pointY + 1][1]);
         }
-        if(pointX > 0) {
+        if (pointX > 0) {
             System.out.println("Left ->" + knowledge[pointX - 1][pointY][1]);
         }
-        if(pointX < 3) {
-            System.out.println("Right ->" +  knowledge[pointX + 1][pointY][1]);
+        if (pointX < 3) {
+            System.out.println("Right ->" + knowledge[pointX + 1][pointY][1]);
         }
 
 
--- src/Wumpus.java
+++ src/Wumpus.java
@@ -2,61 +2,61 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Wumpus {
-    public static double[][][] wumpus(double[][][] knowledge, int[][][] map, int x, int y, int stench){
+    public static double[][][] wumpus(double[][][] knowledge, int[][][] map, int x, int y, int stench) {
 
-        if(stench==1){ //in case agent smells a stench
+        if (stench == 1) { //in case agent smells a stench
             Queue<Integer> w = new LinkedList<>();
 
             int counter = 0;
-            if(knowledge[x-1][y][1] != 0 && (x > 0)) {
-                w.add(x-1);
+            if (knowledge[x - 1][y][1] != 0 && (x > 0)) {
+                w.add(x - 1);
                 w.add(y);
                 counter++;
             }
-            if( knowledge[x+1][y][1] != 0 && (x < 3)) {
-                w.add(x+1);
+            if (knowledge[x + 1][y][1] != 0 && (x < 3)) {
+                w.add(x + 1);
                 w.add(y);
                 counter++;
             }
-            if(knowledge[x][y-1][1] != 0 && (y > 0)) {
+            if (knowledge[x][y - 1][1] != 0 && (y > 0)) {
                 w.add(x);
-                w.add(y-1);
+                w.add(y - 1);
                 counter++;
             }
-            if(knowledge[x][y+1][1] != 0 && (y < 3)) {
+            if (knowledge[x][y + 1][1] != 0 && (y < 3)) {
                 w.add(x);
-                w.add(y+1);
+                w.add(y + 1);
                 counter++;
             }
 
 
-            while(w.peek() != null){
+            while (w.peek() != null) {
                 int a = w.remove();
                 int b = w.remove();
-                knowledge[a][b][1] = (double) 1/counter;
+                knowledge[a][b][1] = (double) 1 / counter;
             }
 
 
-            for(x = 0; x < 4; x++){
-                for(y = 0; y < 4; y++){
-                    if(knowledge[x][y][1] < (double) 1/counter){
+            for (x = 0; x < 4; x++) {
+                for (y = 0; y < 4; y++) {
+                    if (knowledge[x][y][1] < (double) 1 / counter) {
                         knowledge[x][y][1] = 0;
                     }
                 }
             }
-        }else{
+        } else {
 
-            if(x>0) {
-                knowledge[x-1][y][1] = 0;
+            if (x > 0) {
+                knowledge[x - 1][y][1] = 0;
             }
-            if(x<3) {
-                knowledge[x+1][y][1] = 0;
+            if (x < 3) {
+                knowledge[x + 1][y][1] = 0;
             }
-            if(y>0) {
-                knowledge[x][y-1][1] = 0;
+            if (y > 0) {
+                knowledge[x][y - 1][1] = 0;
             }
-            if(y<3) {
-                knowledge[x][y+1][1] = 0;
+            if (y < 3) {
+                knowledge[x][y + 1][1] = 0;
             }
         }
 
